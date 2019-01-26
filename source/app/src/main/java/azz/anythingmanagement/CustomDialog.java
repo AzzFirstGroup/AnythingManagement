1724739a037caf1ba1a6112a99eae8bae2e491a1
saiya <hirano_toyosaki@yahoo.co.jp>
2019-01-26 15:20:58 +0900

    一旦コミット（レイアウト修正中）

--- source/app/src/main/java/azz/anythingmanagement/CustomDialog.java
+++ source/app/src/main/java/azz/anythingmanagement/CustomDialog.java
@@ -5,10 +5,20 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CustomDialog extends DialogFragment {
+
+    String title1="登録確認ダイアログ";
+    String title2="画像取得ダイアログ";
+    String title3="削除確認ダイアログ";
+    String title4="破棄確認ダイアログ";
+
 
     // ダイアログが生成された時に呼ばれるメソッド ※必須
     public Dialog onCreateDialog(Bundle savedInstanceState){
@@ -17,9 +27,16 @@ public class CustomDialog extends DialogFragment {
 
         String dialogTag = this.getTag();
         switch(dialogTag) {
-            case "dialog1":
-                // タイトル設定
-                dialogBuilder.setTitle("登録確認ダイアログ");
+            case "dialog1": // 登録確認ダイアログ
+
+                String title = title1;
+                TextView textView = new TextView(getActivity());
+                // タイトル部分を編集
+                textView = TitleStyle(title);
+//                zdivider.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_light));
+                // タイトル部分を設定
+                dialogBuilder.setCustomTitle(textView);
+
                 // 表示する文章設定
                 dialogBuilder.setMessage("登録しますか？");
 
@@ -28,7 +45,7 @@ public class CustomDialog extends DialogFragment {
 
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // トーストを出す
+                        // TODO 登録画面へ遷移(現在：トーストを出すのみ)
                         Toast toast = Toast.makeText(getActivity(), "登録されました。", Toast.LENGTH_SHORT);
                         toast.show();
                     }
@@ -39,9 +56,14 @@ public class CustomDialog extends DialogFragment {
 
                 break;
 
-            case "dialog2":
-                // タイトル設定
-                dialogBuilder.setTitle("画像取得ダイアログ");
+            case "dialog2": // 画像取得ダイアログ
+
+                title = title2;
+                textView = new TextView(getActivity());
+                // タイトル部分を編集
+                textView = TitleStyle(title);
+                // タイトル部分を設定
+                dialogBuilder.setCustomTitle(textView);
                 // 表示する文章設定
                 dialogBuilder.setMessage("どのパターンのダイアログを表示しますか？");
 
@@ -50,7 +72,7 @@ public class CustomDialog extends DialogFragment {
 
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // トーストを出す
+                        // TODO カメラ起動処理(現在：トーストを出すのみ)
                         Toast toast = Toast.makeText(getActivity(), "カメラ起動", Toast.LENGTH_SHORT);
                         toast.show();
                     }
@@ -61,7 +83,7 @@ public class CustomDialog extends DialogFragment {
 
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // トーストを出す
+                        // TODO フォルダを開く処理(現在：トーストを出すのみ)
                         Toast toast = Toast.makeText(getActivity(), "フォルダを開きます", Toast.LENGTH_SHORT);
                         toast.show();
                     }
@@ -76,18 +98,23 @@ public class CustomDialog extends DialogFragment {
                 });
                 break;
 
-            case "dialog3":
-                // タイトル設定
-                dialogBuilder.setTitle("削除確認ダイアログ");
+            case "dialog3": // 削除確認ダイアログ
+
+                title = title3;
+                textView = new TextView(getActivity());
+                // タイトル部分を編集
+                textView = TitleStyle(title);
+                // タイトル部分を設定
+                dialogBuilder.setCustomTitle(textView);
                 // 表示する文章設定
                 dialogBuilder.setMessage("削除しますか？");
 
-                // 登録確認ボタン作成
+                // 削除確認ボタン作成
                 dialogBuilder.setPositiveButton("はい", new DialogInterface.OnClickListener() {
 
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // トーストを出す
+                        // TODO 削除処理(現在：トーストを出すのみ)
                         Toast toast = Toast.makeText(getActivity(), "削除されました。", Toast.LENGTH_SHORT);
                         toast.show();
                     }
@@ -98,18 +125,23 @@ public class CustomDialog extends DialogFragment {
 
                 break;
 
-            case "dialog4":
-                // タイトル設定
-                dialogBuilder.setTitle("破棄確認ダイアログ");
+            case "dialog4": //破棄確認ダイアログ
+
+                title = title4;
+                textView = new TextView(getActivity());
+                // タイトル部分を編集
+                textView = TitleStyle(title);
+                // タイトル部分を設定
+                dialogBuilder.setCustomTitle(textView);
                 // 表示する文章設定
                 dialogBuilder.setMessage("破棄しますか？");
 
-                // 登録確認ボタン作成
+                // 破棄確認ボタン作成
                 dialogBuilder.setPositiveButton("はい", new DialogInterface.OnClickListener() {
 
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // トーストを出す
+                        // TODO 破棄処理(現在：トーストを出すのみ)
                         Toast toast = Toast.makeText(getActivity(), "破棄されました。", Toast.LENGTH_SHORT);
                         toast.show();
                     }
@@ -125,6 +157,19 @@ public class CustomDialog extends DialogFragment {
         return dialogBuilder.create();
     }
 
+    private TextView TitleStyle(String titleText){
+        // タイトル部分のTextView
+        TextView textView = new TextView(getActivity());
+        // タイトルの文字色
+        textView.setTextColor(ContextCompat.getColor(getActivity(), android.R.color.holo_blue_light));
+
+
+        //文字サイズ
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
+        //タイトルテキスト設定
+        textView.setText(titleText);
+        return textView;
+    }
     private void NgButton(AlertDialog.Builder dialogBuilder){
         // NGボタン作成
         dialogBuilder.setNegativeButton("いいえ", new DialogInterface.OnClickListener(){
